d1c8a518242c732fa33ea62853647dca9acb5cee
Duncan Wyrwas <32854279+dwyrwas@users.noreply.github.com>
2021-11-10 20:11:46 -0600

    Create script.js

---
+++ numberGuesser/script.js
@@ -0,0 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+const generateTarget = () => Math.floor(Math.random() * 9);
+const compareGuesses = (humanGuess, compGuess, secretNumber) => {
+  
+};
